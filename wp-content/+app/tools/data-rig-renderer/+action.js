0ef9f3c92cbf8ab77002007b177c68b1b338f9eb
jiaSheng <50891910+sxxov@users.noreply.github.com>
2025-11-30 17:48:22 +0800

    Add internal tool to render perspectives for splat optimisation

---
+++ wp-content/+app/tools/data-rig-renderer/+action.js
@@ -0,0 +1,4 @@
+import { RigRendererBehavior } from './RigRendererBehavior.js';
+import { registerGlobalBehaviors } from '/+std/behavioral/behavior.js';
+
+registerGlobalBehaviors(RigRendererBehavior);
