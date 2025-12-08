cd948fbd096ee6ed590cee1b7766eb072beae412
jiaSheng <50891910+sxxov@users.noreply.github.com>
2025-12-08 13:44:00 +0800

    Include fonts via link tags

---
+++ wp-content/+app/fonts/marlide/+include.php
@@ -0,0 +1,10 @@
+<?php
+
+use function bare\utilities\url\get_uri;
+
+add_action('wp_print_styles', function () {
+?>
+	<link rel="preload" href="<?= get_uri(__DIR__ . '/m.woff2') ?>" as="font" type="font/woff2">
+	<link rel="stylesheet" href="<?= get_uri(__DIR__ . '/m.css') ?>">
+<?php
+});
