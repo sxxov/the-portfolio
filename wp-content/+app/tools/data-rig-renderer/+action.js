bd5305df1e64811086d67a2c2d0a0217f0591203
jiaSheng <50891910+sxxov@users.noreply.github.com>
2026-01-01 23:40:04 +0800

    Remove temporary rig renderer tool

--- wp-content/+app/tools/data-rig-renderer/+action.js
+++
@@ -1,4 +0,0 @@
-import { RigRendererBehavior } from './RigRendererBehavior.js';
-import { registerGlobalBehaviors } from '/+std/behavioral/behavior.js';
-
-registerGlobalBehaviors(RigRendererBehavior);
