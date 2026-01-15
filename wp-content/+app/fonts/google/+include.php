269ea4c0618f7a715580f9d1093b5561d7264971
jiaSheng <50891910+sxxov@users.noreply.github.com>
2026-01-15 22:38:20 +0800

    Switch font to Outfit, Marlide, & Ofform

--- wp-content/+app/fonts/google/+include.php
+++ wp-content/+app/fonts/google/+include.php
@@ -6,6 +6,6 @@ add_action('wp_print_styles', function () {
 ?>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Monsieur+La+Doulaise&family=Space+Grotesk:wght@300..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Space+Grotesk:wght@300..700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap">
 <?php
 });
