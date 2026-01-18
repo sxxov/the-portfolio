4888f2ca864d80420995c7468addb9fcd7d5ea8a
jiaSheng <50891910+sxxov@users.noreply.github.com>
2026-01-19 06:43:40 +0800

    Rework progress system & begrudgingly add a preloader/splash

--- wp-content/+app/delivery/progress/data-progress-bar/ProgressBarBehavior.js
+++ wp-content/+app/delivery/progress/data-progress-bar/ProgressBarBehavior.js
@@ -1,35 +1,12 @@
 import { ProgressBehavior } from '../data-progress/ProgressBehavior.js';
-import { progress } from '../progress.js';
-import { SmoothingSignal } from '/+app/animation/smooth/SmoothingSignal.js';
 import { attachBehavior, behavior, t } from '/+std/behavioral/behavior.js';
-import { lerp } from '/+std/math/lerp.js';
-import { subscribe } from '/+std/signal/Signal.js';
 
 export const ProgressBarBehavior = behavior(
 	'progress-bar',
 	class {
-		bar = new SmoothingSignal(
-			progress.get(),
-			{
-				epsilon: 0.001,
-				speedPerSecond: 67,
-				smoothingFactor: 0.01,
-			},
-			({ set, seek }) =>
-				subscribe({ progress }, ({ $progress }) => {
-					if ($progress >= 1) {
-						set(1);
-						return;
-					}
-
-					if ($progress <= 0) seek(0);
-					set(lerp($progress, 0.3, 1));
-				}),
-		);
-		progress = t.number.transient.styling.attributing.in(this.bar);
-		visible = t.boolean.transient.attributing.in(
-			this.bar.derive((v) => v > 0 && v < 1),
-		);
+		align = t.string.choices('start', 'center', 'end').default('start');
+		direction = t.number.styling.choices(-1, 1).default(1);
+		axis = t.string.choices('x', 'y').default('x');
 	},
-	(element) => { attachBehavior(element, ProgressBehavior); },
+	(element) => attachBehavior(element, ProgressBehavior),
 );
--- wp-content/+app/delivery/progress/data-progress/ProgressBehavior.js
+++ wp-content/+app/delivery/progress/data-progress/ProgressBehavior.js
@@ -1,16 +1,42 @@
 import { progress, progressBar } from '../progress.js';
+import { SmoothingSignal } from '/+app/animation/smooth/SmoothingSignal.js';
 import { behavior, t } from '/+std/behavioral/behavior.js';
+import { lerp } from '/+std/math/lerp.js';
+import { subscribe } from '/+std/signal/Signal.js';
 
 export const ProgressBehavior = behavior(
 	'progress',
 	class {
 		'' = t.number.transient.styling.attributing.in(progress);
-		interpolated = t.number.transient.styling.attributing.in(progressBar);
-		string = t.string.transient.styling.attributing.in(
-			progressBar.derive((v) => `'${Math.round(v * 100)}'`),
-		);
 		done = t.boolean.transient.attributing.in(
 			progress.derive((v) => v >= 1),
+		);
+
+		interpolated = t.number.transient.styling.attributing.default(0).in(
+			new SmoothingSignal(
+				progress.get(),
+				{
+					epsilon: 0.001,
+					speedPerSecond: 67,
+					smoothingFactor: 0.01,
+				},
+				({ set, seek }) =>
+					subscribe({ progress }, ({ $progress }) => {
+						if ($progress >= 1) {
+							set(1);
+							return;
+						}
+
+						if ($progress <= 0) seek(0);
+						set(lerp($progress, 0.3, 1));
+					}),
+			),
+		);
+		interpolatedVisible = t.boolean.transient.attributing.in(
+			this.interpolated.derive((v) => v > 0 && v < 1),
+		);
+		interpolatedText = t.string.transient.styling.attributing.in(
+			this.interpolated.derive((v) => `${Math.round(v * 100)}`),
 		);
 	},
 );
