26a70ac355de59cdb954125dd29a5a33e6768e9d
jiaSheng <50891910+sxxov@users.noreply.github.com>
2026-09-01 03:33:12 +0800

    Reuse `progressBar` in `progress.js`

--- wp-content/+app/delivery/progress/data-progress/ProgressBehavior.js
+++ wp-content/+app/delivery/progress/data-progress/ProgressBehavior.js
@@ -1,8 +1,5 @@
 import { progress, progressBar } from '../progress.js';
-import { SmoothingSignal } from '/+app/animation/smooth/SmoothingSignal.js';
 import { behavior, t } from '/+std/behavioral/behavior.js';
-import { lerp } from '/+std/math/lerp.js';
-import { subscribe } from '/+std/signal/Signal.js';
 
 export const ProgressBehavior = behavior(
 	'progress',
@@ -12,26 +9,9 @@ export const ProgressBehavior = behavior(
 			progress.derive((v) => v >= 1),
 		);
 
-		interpolated = t.number.transient.styling.attributing.default(0).in(
-			new SmoothingSignal(
-				progress.get(),
-				{
-					epsilon: 0.001,
-					speedPerSecond: 67,
-					smoothingFactor: 0.01,
-				},
-				({ set, seek }) =>
-					subscribe({ progress }, ({ $progress }) => {
-						if ($progress >= 1) {
-							set(1);
-							return;
-						}
-
-						if ($progress <= 0) seek(0);
-						set(lerp($progress, 0.3, 1));
-					}),
-			),
-		);
+		interpolated = t.number.transient.styling.attributing
+			.default(0)
+			.in(progressBar);
 		interpolatedVisible = t.boolean.transient.attributing.in(
 			this.interpolated.derive((v) => v > 0 && v < 1),
 		);
